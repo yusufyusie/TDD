21224f471d0aff6c0a905c11d446ddb871e9ed5f
dvcorreia1984 <victor@onsleer.co.za>
2023-10-28 20:36:52 +0200

    Added test for class

--- solver_spec.rb
+++ solver_spec.rb
@@ -4,6 +4,12 @@ require_relative 'solver'
 describe Solver do
   let(:solver) { Solver.new }
 
+  describe '#classSolver' do
+    it 'must have a class' do
+      expect(solver.class).to eq(Solver)
+    end
+  end
+
   describe '#factorial' do
     it 'returns 1 for factorial of 0' do
       expect(solver.factorial(0)).to eq(1)
